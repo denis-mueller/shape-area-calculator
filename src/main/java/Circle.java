aceed9d1052895d248d23a5d3b4f3e44dff955ea
Denis Müller <denismueller.private@gmail.com>
2018-10-18 16:28:42 +0200

    Create circle class

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,10 +1,21 @@
-public class Circle extends Shape {
-    double radius;
-    Circle(double radius){
-        this.radius = radius;
+/**
+ * The Circle class has attributes of a circle saved in a HashMap.
+ * It has the attribute radius.
+ * The attribute can be updated by using the {@link Shape#area() area()} method.
+ */
+class Circle extends Shape {
+    Circle() {
+        attributes.put("radius", null);
     }
 
-    double area(){
+    /**
+     * Calculates the area of a circle.
+     *
+     * @return area of the circle
+     */
+    double area() {
+        double radius = attributes.get("radius");
+
         return radius * radius * Math.PI;
     }
 }
