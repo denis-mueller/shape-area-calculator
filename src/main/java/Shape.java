15e9357940b93f1157096a5cca6000f2c02a8d0b
Denis Müller <denismueller.private@gmail.com>
2018-10-18 16:30:18 +0200

    Create shape class

--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -1,3 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 abstract class Shape {
+    final Map<String, Double> attributes = new HashMap<>();
+
     abstract double area();
+
+    /**
+     * Checks whether a shape has an attribute and if it does it sets the value for the attribute.
+     *
+     * @param attributeName  name of the attribute as string
+     * @param attributeValue value of the attribute as double
+     */
+    void setAttribute(String attributeName, double attributeValue) {
+        if (attributes.containsKey(attributeName)) attributes.put(attributeName, attributeValue);
+    }
 }
